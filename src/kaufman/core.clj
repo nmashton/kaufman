aa4d5000f1d69473ffcb20340c8c4fa9db9d15ba
Neil Ashton <neil.m.ashton@gmail.com>
2016-10-24 16:25:11 -0400

    Shucking out some obsolete stuff from core.clj

--- src/kaufman/core.clj
+++ src/kaufman/core.clj
@@ -2,30 +2,9 @@
 ;; Produces both a map mirroring the hierarchical structure of the file, `processed`,
 ;; and a flat list of maps suitable for importing into a relational database, `relational`.
 (ns kaufman.core
-  (:use [kaufman.actions])
-  (:use [cheshire.core]))
+  (:use [kaufman.actions]))
 
 ;; The core sequence of transformations.
-;; To understand what these do, consult [kaufman.actions](#kaufman.actions).
-; (def processed
-;   (->
-;    "./data/kaufman-cleaned.txt"
-;    get-raw-data
-;    strip-pagenums-footers
-;    strip-percents
-;    group-by-percent-delimiters
-;    split-on-xx-delimiters
-;    group-by-xx-keys
-;    flatten-spurious-sequences
-;    split-on-eq-delimiters
-;    eliminate-garbage
-;    eliminate-more-garbage
-;    eliminate-even-more-garbage
-;    group-by-root-lines
-;    split-on-blank-lines
-;    turn-lines-into-maps
-;    remove-blank-lexemes))
-
 (def processed-tr
   (eduction
     (comp
@@ -41,12 +20,3 @@
       turn-lines-into-maps-tr
       remove-blank-lexemes-tr)
     (get-raw-data "./data/kaufman-footers-stripped.txt")))
-
-;; The "relational" encoding of `processed`.
-; (def relational
-;   (convert-to-relational processed))
-
-;; We can write the result to a JSON file, using Cheshire.
-; (comment
-;   (spit "./data/kaufman-processed.json"
-;         (generate-string processed {:pretty false})))
