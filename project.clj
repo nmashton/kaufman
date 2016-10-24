3ed5384c31a589a2727fbcaac53246b670b95675
Neil Ashton <neil.m.ashton@gmail.com>
2016-10-24 17:33:13 -0400

    Fixing dependencies, re-generating docs

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject kaufman-justeson "0.0.1"
   :description "Kaufman & Justeson 2003 processor"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "4.0.2"]
-                 [prismatic/plumbing "0.5.3"]]
-  :dev-dependencies [[lein-marginalia "0.9.0"]
-                     [proto-repl "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dev-dependencies [[lein-marginalia "0.9.0"]]
   :jvm-opts ["-Xms256m" "-Xmx1024m"]
   :plugins [[lein-marginalia "0.9.0"]])
